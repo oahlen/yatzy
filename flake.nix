368b15da0e63944437031a8398b2fd5b3bae44b9
Oscar Ahlén <github@ahlen.dev>
2023-10-16 20:15:51 +0200

    Add Nix Flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Simple yatzy app built with ASP.NET and Svelte";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (with dotnetCorePackages;
+              combinePackages [
+                sdk_7_0
+                sdk_6_0
+              ])
+            netcoredbg
+            omnisharp-roslyn
+            nodejs_20
+            nodePackages.npm
+            nodePackages.svelte-language-server
+          ];
+        };
+      }
+    );
+}
